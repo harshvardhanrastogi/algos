2226ba51ccb227ee0d65059342248a2fb18502a8
Harsh Rastogi <harshrajan00@gmail.com>
2020-07-13 01:21:21 +0530

    Add delete method, fix search method

--- Trie.java
+++ Trie.java
@@ -47,15 +47,12 @@ public class Trie {
 			TrieNode root = this.root;
 			if (root == null) return false;
 			int count = 0;
-			char[] chars = word.toCharArray();
-			while (count <= word.length()) {
-				if (root.endOfWord) return true;
-				char c = chars[count];
-				if (!root.children.containsKey(c)) return false;
-				root = root.children.get(c);
+			while (count < word.length()) {
+				root = root.children.get(word.charAt(count));
+				if(root == null) return false;
 				count++;
 			}
-			return false;
+			return root.endOfWord;
 		} else {
 			return prefixSearch(word);
 		}
@@ -81,36 +78,76 @@ public class Trie {
 
 
 	public void delete(String word) {
-		delete(this.root, word, 0);
+		Mutable b = new Mutable(false);
+		delete(this.root, word, 0, b);
 	}
 
-	private void delete(TrieNode root, String word, int index) {
-
-		if (index < word.length() - 1) {
-			try {
-				System.out.println(word.charAt(index));
-				index++;
-				delete(root.children.get(word.charAt(index)), word, index);
-			} catch(NullPointerException e) {
-				e.printStackTrace();
-				Iterator<Map.Entry<Character, TrieNode>> it = root.children.entrySet().iterator();
-				while (it.hasNext()) {
-					Map.Entry<Character, TrieNode> entry = it.next();
-					System.out.println(entry.getKey() +" "+entry.getValue());
-				}
-			}
+	private void delete(TrieNode root, String word, int index, Mutable del) {
+		if (index < word.length()) {
+			delete(root.children.get(word.charAt(index)), word, ++index, del);//Depth first traversal
 		}
 
-		TrieNode nodeToDelete = root.children.get(word.charAt(index)).children.get(word.charAt(index + 1));
-		if (index == word.length() - 2 && !nodeToDelete.children.isEmpty()) {
-			nodeToDelete.endOfWord = false;
+		if (del.abort == 1) {
 			return;
-		} 
+		}
+		
+		if (!del.b && index == word.length() && !root.children.isEmpty() && root.endOfWord) {
+			root.endOfWord = false;//remove the end flag 
+			del.abort = 1;// stop all the method stack calls if this word is a substring of another word
+		}
 
-		// if (nodeToDelete.children.isEmpty()) {
-		// 	root.children.remove(word.charAt(index));
-		// }
+		if (root.children.isEmpty()) {
+			del.b = true;//last empty node reached start removing keys
+		}
+
+		if (del.b) {
+			if (root.children.size() > 1) {// node has more than 1 key
+				del.b = false;
+				//remove key if it does not appear in another word or it is the last character
+				if(!root.children.get(word.charAt(index - 1)).endOfWord || (index - 1) == word.length() - 1) {
+					root.children.remove(word.charAt(index - 1));				
+				}
+			} else {
+				root.children.clear();
+			}
+		}
 	}
+
+	static class Mutable {
+		public boolean b; 
+		public int abort;
+		public Mutable(boolean b, int abort) {
+			this.b = b;
+			this.abort = abort;
+		}
+		public Mutable(boolean b) {
+			this.b = b;
+		}
+		public Mutable(int abort) {
+			this.abort = abort;
+		}
+		public String toString() {
+			return String.valueOf(b);
+		}
+	}
+
+	public void printInOrder(TrieNode root) {
+		if (root.children.isEmpty()) return;
+
+		Queue<TrieNode> q = new LinkedList<>();
+		q.add(root);
+
+		System.out.println();
+		TrieNode n;
+		while((n = q.poll()) != null) {
+			System.out.println(n.children.keySet() +" "+n.endOfWord);
+			Iterator<Character> it = n.children.keySet().iterator();
+			while(it.hasNext()) {
+				q.add(n.children.get(it.next()));
+			}
+		}
+	}
+
 
 	public static void main(String[] args) {
 		Trie trie = new Trie();
@@ -118,8 +155,13 @@ public class Trie {
 		trie.insert("abgl");
 		trie.insert("cdf");
 		trie.insert("abcd");
+		trie.insert("lmo");
 		trie.insert("lmn");
+
+		trie.delete("abcd");
 		trie.delete("abc");
-		System.out.println("Has abc "+trie.search("abc", true));
+		
+		
+		trie.printInOrder(trie.getRoot());
 	}
 }
